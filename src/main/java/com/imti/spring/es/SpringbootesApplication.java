c458696a17b3af8a188a58c7a545e8bb8e3bfbc7
Imteyaz Ahmad <imteyaz.ahmad@culturemachine.in>
2016-01-28 09:56:34 +0530

    Spring boot Elasticsearch Project

---
+++ src/main/java/com/imti/spring/es/SpringbootesApplication.java
@@ -0,0 +1,12 @@
+package com.imti.spring.es;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootesApplication.class, args);
+	}
+}
---
+++ src/test/java/com/imti/spring/es/SpringbootesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.imti.spring.es;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SpringbootesApplication.class)
+public class SpringbootesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
